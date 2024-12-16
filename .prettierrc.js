f0ab66af26c9a68cc60fc8dbd0a3f4971aee4d21
Jan Wiebe <janwiebe@jump.nl>
2024-12-16 15:59:35 +0100

    add prettier and recommended plugins

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  endOfLine: 'auto',
+  printWidth: 100,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  quoteProps: 'consistent',
+}
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import "expo-dev-client";
+import 'expo-dev-client'
 
-import { registerRootComponent } from "expo";
+import { registerRootComponent } from 'expo'
 
-import App from "./App";
+import App from './App'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App)
